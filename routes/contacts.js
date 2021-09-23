91992e138a78abfc03ec6934aa381aa1ced1d8d7
Glagny <nikoicardo@gmail.com>
2021-09-23 13:38:02 -0600

    Added Contact Schema and used it to implement the get contacts request.

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    get all user's contacts
 // @access  Private
 
-router.get('/', (req, res) => {
-  res.send('Get contacts.');
+router.get('/', auth, async (req, res) => {
+  try {
+    // sorting by date: -1 will show the most recent contacts first.
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   Post api/contacts
 // @desc    post a contact
 // @access  Private
 
-router.post('/', (req, res) => {
+router.post('/', auth, (req, res) => {
   res.send('Add contact');
 });
 
@@ -21,7 +34,7 @@ router.post('/', (req, res) => {
 // @desc    update a contact
 // @access  Private
 
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, (req, res) => {
   res.send('Update Contact.');
 });
 
@@ -29,7 +42,7 @@ router.put('/:id', (req, res) => {
 // @desc    delete a contact
 // @access  Private
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
   res.send('Delete contact.');
 });
 
