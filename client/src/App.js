da087658a4c1501cab1855981831b3333ca4575c
Glagny <nikoicardo@gmail.com>
2021-09-28 11:38:03 -0600

    Add AlertContext and Alert System. Add Alerts to User Registration form.

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,33 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import ContactState from './context/Contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
 
 import './App.css';
 
 const App = () => {
   return (
-    <AuthState>
-      <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />\
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </ContactState>
-    </AuthState>
+    <ContactState>
+      <AuthState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />\
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </AuthState>
+    </ContactState>
   );
 };
 
