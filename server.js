9e2f63c1d4b3c9a4d30c84e604dd9ea4d5d940ec
Glagny <nikoicardo@gmail.com>
2021-09-30 10:23:08 -0600

    Bugfix

--- server.js
+++ server.js
@@ -27,5 +27,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
+console.log(PORT);
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}.`));
